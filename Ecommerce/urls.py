b2175986baaacecaa190b172eaddec68cb256dfe
Muhammad Nouman <59723761+NoumanHere@users.noreply.github.com>
2020-07-03 11:43:42 +0500

    Update urls.py

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -10,8 +10,4 @@ urlpatterns = [
     path('', include('core.urls',namespace = 'core'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
